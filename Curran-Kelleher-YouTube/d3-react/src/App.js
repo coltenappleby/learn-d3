ae82f5a4fa44340a623270e604c8adb52d4c5f6e
ColtenAppleby <Colten.appleby@gmail.com>
2022-05-19 13:36:06 -0400

    manual pie chart

--- Curran-Kelleher-YouTube/d3-react/src/App.js
+++ Curran-Kelleher-YouTube/d3-react/src/App.js
@@ -1,32 +1,48 @@
 import "./App.css";
 import React, { useEffect, useState, useCallBack } from "react";
-import * as d3 from "d3";
+import { csv, arc } from "d3";
 
 const csvUrl =
   "https://gist.githubusercontent.com/coltenappleby/bf283f12acc2bbb2ddc83ecf7b07441b/raw/colors.csv";
 const width = 960;
 const height = 500;
+const centerX = width/2;
+const centerY = height/2;
+
+const pieArc = arc()
+  .innerRadius(0)
+  .outerRadius(width)
 
 const App = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    d3.csv(csvUrl).then(setData);
+    csv(csvUrl).then(setData);
   }, []);
 
   if (!data) {
     return <pre>Loading...</pre>;
   }
 
-  return data.map((d) => (
-    <div 
-      style={{
-        backgroundColor: d["Hex"],
-        width: "100px",
-        height: "1px",
-      }}>
-    </div>
-  ));
+  return (
+    <svg width={width} height={height} >
+      <g transform={`translate(${centerX}, ${centerY})`}>
+      {data.map((d,i) => (
+        <path 
+          fill={d["Hex"]} 
+          d={ pieArc({
+            startAngle: i / data.length *2 * Math.PI, 
+            endAngle: (i+1) / data.length *2 * Math.PI   
+          })}
+        />
+
+      ))}
+      </g>
+    </svg>
+  )
+  
+  
+
 };
 
 export default App;
