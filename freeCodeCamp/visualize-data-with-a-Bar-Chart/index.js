4b2a169b20c1c48faa2d74ffd219798cf6265bf3
Colten Appleby <colten.appleby@gmail.com>
2022-09-14 12:02:14 -0400

    get + format data
    * d3.sjon
    * formatted data with new Date

--- freeCodeCamp/visualize-data-with-a-Bar-Chart/index.js
+++ freeCodeCamp/visualize-data-with-a-Bar-Chart/index.js
@@ -9,28 +9,39 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
-    let rawData;
+    // Size of plot
+    const w = 1000
+    const h = 1000
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => rawData = JSON.stringify(data))
-        .then(() => console.log(rawData))
-        .then(() => {
+    d3.json(url)
+        .then((rawData) => {
 
-            
-            
-            
-            const data = rawData.data
-            console.log(data)
-            // Size of plot
-            const w = 1000
-            const h = 1000
-            
+            const data = rawData.data.map((event)=>{
+                const date = new Date(event[0])
+                const year = event[0].substring(0,4)
+                let q
+                if(event[0].substring(5,6) ==  '01'){
+                    q = 'Q1'
+                } else if(event[0].substring(5,6) ==  '04'){
+                    q = 'Q2'
+                } else if(event[0].substring(5,6) ==  '07'){
+                    q = 'Q3'
+                } else {
+                    q = 'Q4'
+                }
+
+                return [date, `${year}-${q}`, event[1]]
+
+            })
+        
+
             // Scales
             
-            // const xScale = d3.scaleLinear()
-            //                 .domain()
-            //                 .range()
+            // const xScale = d3.scaleOrdinal()
+            //     .domain([d3.min(data.data, d=> d[0]), d3.max(data.data, d=>d[0])])
+            //     .range([0, w])
         })
+            
+    
     
   });
