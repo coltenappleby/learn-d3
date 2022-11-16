5b6764c50522ad57abe5581d75ed87302b233f08
Colten Appleby <colten.appleby@gmail.com>
2022-11-15 23:17:35 -0500

    Treemap legend complete

--- freeCodeCamp/Treemap/index.js
+++ freeCodeCamp/Treemap/index.js
@@ -42,6 +42,14 @@ document.addEventListener('DOMContentLoaded', function(){
         .style("opacity", 0)
         .style("padding", "5px")   
 
+    let legend = d3.select("body")
+        .append("svg")
+            .attr('id', 'legend')
+            .attr("height", 300)
+            .attr("width", 500)
+            .attr('transform', `translate(${0}, ${0})`)
+
+
     
     d3.json(url).then((rawData) => { 
 
@@ -54,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
         d3.treemap()
             .size([width, height])
-            .padding(4)
+            .padding(1)
         (root)
 
         const colors = d3.scaleOrdinal(d3.schemeAccent)
@@ -71,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function(){
             .enter()
             .append('g')
             .attr('class', 'node-group')
-            .attr('transform', d => `translate(${d.x0}, ${d.y0})`)
+            .attr('transform', d => `translate(${d.x0 + margin.left}, ${d.y0+margin.top})`)
 
         nodes
             .append('rect')
@@ -95,41 +103,40 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
         // Mike Bostock Code
-    function wrap(text) {
-        console.log(text)
-        text.each(function() {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            width = parseFloat(text.attr('width'));
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            y = text.attr("y"),
-            dy = parseFloat(text.attr("dy")),
-            tspan = text
-                .text(null)
-                .append("tspan")
-                .attr("x", 2)
-                .attr("y", y)
-                .attr("dy", dy + "em");
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text
-                    .append("tspan")
-                    .attr("x", 2)
-                    .attr("y", y)
-                    .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                    .text(word);
-            }
+        function wrap(text) {
+            text.each(function() {
+                var text = d3.select(this),
+                    words = text.text().split(/\s+/).reverse(),
+                    word,
+                    width = parseFloat(text.attr('width'));
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = text.attr("y"),
+                    dy = parseFloat(text.attr("dy")),
+                    tspan = text
+                        .text(null)
+                        .append("tspan")
+                        .attr("x", 2)
+                        .attr("y", y)
+                        .attr("dy", dy + "em");
+                while (word = words.pop()) {
+                    line.push(word);
+                    tspan.text(line.join(" "));
+                    if (tspan.node().getComputedTextLength() > width) {
+                        line.pop();
+                        tspan.text(line.join(" "));
+                        line = [word];
+                        tspan = text
+                            .append("tspan")
+                            .attr("x", 2)
+                            .attr("y", y)
+                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                            .text(word);
+                    }
+                }
+            });
         }
-        });
-    }
 
         nodes
             .on("mousemove", (e,d)=> {
@@ -149,6 +156,42 @@ document.addEventListener('DOMContentLoaded', function(){
             .on("mouseout", () => {
                 tooltip.transition().duration(200).style('opacity', 0)               
             })
+            
+    
+        // Lets make the Legend
+            
+        const categories = root.children.map(d => d.data.name)
+
+        const V_SPACING = 40;
+        const H_SPACING = 100;
+        const cols = Math.floor(categories.length/5)
+        const RECT_HEIGHT = 20;
+        const RECT_WIDTH = 20;
+
+        let elements = legend.selectAll('g')
+            .data(categories)
+            .enter()
+            .append('g')
+            .attr('transform', (d,i) => {
+                return(
+                    `translate(${i%cols * H_SPACING}, ${Math.floor(i / cols) * V_SPACING})`
+                )
+            })
+
+        elements//.selectAll('g')
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('height', RECT_HEIGHT)
+            .attr('width', RECT_WIDTH)
+            .style('fill', d => colors(d))
+            .attr('class', 'legend-rect')
+        
+        elements.append('text')
+            .attr('x', RECT_WIDTH*1.2)
+            .attr('y', RECT_HEIGHT*.8)
+            .text(d => d)
+            .style('font-size', 15)
     })
 
 
